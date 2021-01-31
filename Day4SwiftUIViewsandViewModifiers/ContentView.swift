579219777542e3cd605affdbde8580ae661a13fe
Abdulmalik Muhammad <bawasuru@gmail.com>
2021-01-31 15:48:08 +0100

    Initial Commit

---
+++ Day4SwiftUIViewsandViewModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day4SwiftUIViewsandViewModifiers
+//
+//  Created by Abdulmalik Muhammad on 31/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
